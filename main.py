d920b162783edefcc9849577cef8137e7a3db0ae
Camelia Groza <camelia.groza1606@yahoo.com>
2021-10-18 16:02:25 +0300

    commit

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def menu():
     print("2. Afisarea listei dupa eliminarea duplicatelor")
     print("3. Afisarea sumei primelor n numere pozitive din lista")
     print("4. Verifica daca toate numerele pozitive din lista sunt in ordine crescatoare")
-    print("5. ")
+    print("5. Afisarea listei obtinute din lista initiala in care numerele care apar doar o singura data "
+          "sunt inlocuite cu numarul de divizori proprii ai numarului")
     print("6. Iesire")
     print("0. Afisare lista")
 
@@ -25,11 +26,11 @@ def eliminare_duplicate(l):
     '''
     rezultat = []
     for x in l:
-        ok = 0
+        ok = 1
         for y in rezultat:
             if x==y:
-                ok = 1
-        if ok == 0:
+                ok = 0
+        if ok:
             rezultat.append(x)
     return rezultat
 
@@ -91,16 +92,60 @@ def verifica_nr_pozitive_ordine_cresc(l):
 
 
 def test_verifica_nr_pozitive_ordine_cresc():
+    '''
+    Functie de test
+    '''
     assert verifica_nr_pozitive_ordine_cresc([]) == "DA"
     assert verifica_nr_pozitive_ordine_cresc([-1, -2, -9, -10, -4]) == "DA"
     assert verifica_nr_pozitive_ordine_cresc([10, 13, -1, 24, 33, 45]) == "DA"
     assert verifica_nr_pozitive_ordine_cresc([1, 2, -3, -4, 5, 1, -9]) == "NU"
 
 
+def modificare_lista(l):
+    '''
+    Face o noua lista din lista initiala in care numerele care apar doar o singura data
+    sunt inlocuite cu numarul de divizori proprii ai numarului
+    :param l: o lista de intregi
+    :return: lista obtinuta din l prin inlocuirea numerelor care apar doar o singura data
+    cu numarul de divizori proprii ai numarului
+    '''
+    rezultat = []
+    for i in range(len(l)):
+        x = l[i]
+        cate = 0
+        for j in range(len(l)):
+            if x == l[j]:
+                cate = cate + 1
+        if cate == 1:
+            duplicat = 0
+        else:
+            duplicat = 1
+        if duplicat == 1:
+            rezultat.append(x)
+        else:
+            nr_div = 0
+            for j in range(2,x//2+1):
+                if x % j == 0:
+                    nr_div = nr_div + 1
+            rezultat.append(nr_div)
+    return rezultat
+
+
+def test_modificare_lista():
+    '''
+    Functie de test
+    '''
+    assert modificare_lista([]) == []
+    assert modificare_lista([25, 13, 26, 13, 19]) == [1, 13, 2, 13, 0]
+    assert modificare_lista([2, 6, 6, 25]) == [0, 6, 6, 1]
+
+
+
 def main():
     test_eliminare_duplicate()
     test_suma_primele_n_pozitive()
     test_verifica_nr_pozitive_ordine_cresc()
+    test_modificare_lista()
     l = []
     while True:
         menu()
@@ -114,6 +159,8 @@ def main():
             print(suma_primele_n_pozitive(l, n))
         elif optiune == 4:
             print(verifica_nr_pozitive_ordine_cresc(l))
+        elif optiune == 5:
+            print(modificare_lista(l))
         elif optiune == 6:
             break
         elif optiune == 0:
